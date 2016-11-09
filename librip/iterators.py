a531788c100fe29013d4ee3e82ccb47f5edcd90a
Vovan205 <vovan305@yandex.ru>
2016-11-09 14:20:34 +0300

    Update iterators.py

--- librip/iterators.py
+++ librip/iterators.py
@@ -1,17 +1,36 @@
 # Итератор для удаления дубликатов
 class Unique(object):
     def __init__(self, items, **kwargs):
-        # Нужно реализовать конструктор
-        # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
-        # в зависимости от значения которого будут считаться одинаковые строки в разном регистре
-        # Например: ignore_case = True, Aбв и АБВ разные строки
-        #           ignore_case = False, Aбв и АБВ одинаковые строки, одна из них удалится
-        # По-умолчанию ignore_case = False
-        pass
+        self.items = list(items)
+        self.index = -1
+        self.lst = []
+        self.len = len(self.items)
+        self.ignore_case = kwargs.get('ignore_case')
 
     def __next__(self):
-        # Нужно реализовать __next__    
-        pass
+        self.index += 1
+        if self.index == self.len:
+            raise StopIteration
+        buffer = self.items[self.index]
+        buf_str = str(buffer)
+        if self.ignore_case:
+            buf_str = buf_str.lower()
+            while buf_str in self.lst:
+                self.index += 1
+                if self.index == self.len:
+                    raise StopIteration
+                buffer = self.items[self.index]
+                buf_str = str(buffer).lower()
+            self.lst.append(buf_str)
+            return buffer
+        else:
+            while buffer in self.lst:
+                self.index += 1
+                if self.index == self.len:
+                    raise StopIteration
+                buffer = self.items[self.index]
+            self.lst.append(buffer)
+            return buffer
 
     def __iter__(self):
         return self
